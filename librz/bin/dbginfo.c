660fe92ef5cbfc69fecd91dff5d4e20c419e1812
Florian Märkl <info@florianmaerkl.de>
2021-03-19 14:18:12 +0100

    Add new Line Data Structure

--- librz/bin/dbginfo.c
+++ librz/bin/dbginfo.c
@@ -1,16 +1,297 @@
 // SPDX-FileCopyrightText: 2009-2020 nibble <nibble.ds@gmail.com>
 // SPDX-FileCopyrightText: 2009-2020 pancake <pancake@nopcode.org>
+// SPDX-FileCopyrightText: 2021 thestr4ng3r <info@florianmaerkl.de>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 #include <rz_types.h>
 #include <rz_bin.h>
 
+// TODO: remove this!!!!!!!!!!!!!!!!!!!!!
 RZ_API void rz_bin_source_row_free(RzBinSourceRow *row) {
 	if (!row) {
 		return;
 	}
 	free(row->file);
 	free(row);
+}
+/// !!!!!!!!!!!!!!!!!!!!!!111!!!!!!!!!!!!!!!11
+
+static void source_file_fini(RzBinSourceFile *sf, void *user) {
+	free(sf->file);
+}
+
+RZ_API void rz_bin_source_line_info_builder_init(RzBinSourceLineInfoBuilder *builder) {
+	rz_vector_init(&builder->files, sizeof(RzBinSourceFile), (RzVectorFree)source_file_fini, NULL);
+	rz_vector_init(&builder->lines, sizeof(RzBinSourceLine), NULL, NULL);
+}
+
+RZ_API void rz_bin_source_line_info_builder_fini(RzBinSourceLineInfoBuilder *builder) {
+	rz_vector_fini(&builder->files);
+	rz_vector_fini(&builder->lines);
+}
+
+/**
+ * \param file may be NULL, see RzBinSourceFile for exact meaning
+ */
+RZ_API void rz_bin_source_line_info_builder_push_file_sample(RzBinSourceLineInfoBuilder *builder, ut64 address, RZ_NULLABLE const char *file) {
+	RzBinSourceFile *sample = rz_vector_push(&builder->files, NULL);
+	if (!sample) {
+		return;
+	}
+	sample->address = address;
+	sample->file = file ? strdup(file) : NULL;
+}
+
+/**
+ * \param line may be 0 or a positive line number, see RzBinSourceLine for exact meaning
+ */
+RZ_API void rz_bin_source_line_info_builder_push_line_sample(RzBinSourceLineInfoBuilder *builder, ut64 address, ut32 line, ut32 column) {
+	RzBinSourceLine *sample = rz_vector_push(&builder->lines, NULL);
+	if (!sample) {
+		return;
+	}
+	sample->address = address;
+	sample->line = line;
+	sample->column = column;
+}
+
+static int file_cmp(const void *a, const void *b) {
+	const RzBinSourceFile *fa = a;
+	const RzBinSourceFile *fb = b;
+	return fa->address < fb->address ? -1 : (fa->address > fb->address ? 1 : 0);
+}
+
+static int line_cmp(const void *a, const void *b) {
+	const RzBinSourceLine *fa = a;
+	const RzBinSourceLine *fb = b;
+	return fa->address < fb->address ? -1 : (fa->address > fb->address ? 1 : 0);
+}
+
+RZ_API RzBinSourceLineInfo *rz_bin_source_line_info_builder_build_and_fini(RzBinSourceLineInfoBuilder *builder) {
+	RzBinSourceLineInfo *r = RZ_NEW0(RzBinSourceLineInfo);
+	if (!r) {
+		goto err;
+	}
+	size_t initial_files_count = rz_vector_len(&builder->files); // final count may be less after removing dups
+	if (initial_files_count) {
+		r->files = RZ_NEWS0(RzBinSourceFile, initial_files_count);
+		if (!r->files) {
+			goto err_r;
+		}
+	}
+	size_t initial_lines_count = rz_vector_len(&builder->lines); // final count may be less after removing dups
+	if (initial_lines_count) {
+		r->lines = RZ_NEWS0(RzBinSourceLine, rz_vector_len(&builder->lines));
+		if (!r->lines) {
+			goto err_files;
+		}
+	}
+
+	// samples should be built in flat RzVector to avoid excessive small mallocs,
+	// for sorting we use a pvector with references into our flat vectors (after flushing them).
+
+	if (initial_files_count) {
+		// pack the files.
+		// as an example, this loop would transform this:
+		//
+		//  - 0x100 "middleville.c" // starting at addr 0x100, there is "middleville.c"
+		//  - 0x150 "middleville.c" // starting at addr 0x150, there is "middleville.c"
+		//  - 0x200 NULL            // the previous "middleville.c" ends at 0x200
+		//  - 0x200 "middleville.c" // starting at addr 0x200, there is "middleville.c"
+		//  - 0x300 NULL            // the previous "middleville.c" ends at 0x300
+		//  - 0x300 "arecibo.c"     // starting at addr 0x300, there is "arecibo.c"
+		//  - 0x400 NULL            // the previous "arecibo.c" ends at 0x400
+		//
+		// to this:
+		//
+		//  - 0x100 "middleville.c" // starting at addr 0x100, there is "middleville.c"
+		//  - 0x300 "arecibo.c"     // starting at addr 0x300, there is "arecibo.c"
+		//  - 0x400 NULL            // the previous "arecibo.c" ends at 0x400
+		//
+		// i.e. making sure there is most a single entry at a given address, cancelling out closing
+		// entries if a non-closing entry starts there at the same time removing redundant entries.
+		RzPVector sorter;
+		rz_pvector_init(&sorter, NULL);
+		RzBinSourceFile *initial_files = rz_vector_flush(&builder->files);
+		rz_pvector_reserve(&sorter, initial_files_count);
+		r->files_count = 0;
+		for (size_t i = 0; i < initial_files_count; i++) {
+			rz_pvector_push(&sorter, &initial_files[i]);
+		}
+		rz_pvector_sort(&sorter, file_cmp);
+		// the shadow tracks a sample that has just been considered at the current/previous
+		// address but that was swallowed by the sample before that in order to extend it.
+		// however we must still take this into account as new samples come in at the same address
+		// because if they have a lower priority than this shadowed sample, they must be skipped.
+		ut64 shadow_addr = UT64_MAX;
+		char *shadow_file = NULL;
+		for (size_t i = 0; i < initial_files_count; i++) {
+			RzBinSourceFile *new_file = rz_pvector_at(&sorter, i);
+			if (shadow_addr != UT64_MAX && shadow_addr == new_file->address && (!new_file->file || strcmp(shadow_file, new_file->file) >= 0)) {
+				// the shadow is stronger than this sample
+				free(new_file->file);
+				continue;
+			}
+			if (!r->files_count || r->files[r->files_count - 1].address != new_file->address) {
+				// new address, just move this entry to the final array, ...
+				if (r->files_count) {
+					RzBinSourceFile *prev = &r->files[r->files_count - 1];
+					if ((!prev->file && !new_file->file) || (prev->file && new_file->file && !strcmp(prev->file, new_file->file))) {
+						// ... unless it is identical to the previous
+						if (new_file->file) {
+							shadow_addr = new_file->address;
+							free(shadow_file);
+							shadow_file = new_file->file;
+						}
+						continue;
+					}
+				}
+				r->files[r->files_count++] = *new_file;
+			} else if (new_file->file) {
+				// same address as the previous and we are not a closing sample, decide how to resolve this...
+				RzBinSourceFile *prev = &r->files[r->files_count - 1];
+				if (!prev->file) {
+					// we bring the string!
+					if (r->files_count >= 2 && r->files[r->files_count - 2].file && !strcmp(r->files[r->files_count - 2].file, new_file->file)) {
+						// but actually we just cancel out the previous closing entry and
+						// continue the non-closing one before that.
+						r->files_count--;
+						shadow_addr = new_file->address;
+						free(shadow_file);
+						shadow_file = new_file->file;
+					} else {
+						prev->file = new_file->file;
+					}
+				} else if (strcmp(prev->file, new_file->file) < 0) {
+					// both have a string at the same address, but we only keep one.
+					// strcmp is used to resolve non-determinism from the unstable (possibly randomized) quicksort.
+					if (r->files_count >= 2 && r->files[r->files_count - 2].file && !strcmp(r->files[r->files_count - 2].file, new_file->file)) {
+						// if we would override here, we would have a redundant entry, so we just remove the previous
+						shadow_addr = new_file->address;
+						free(shadow_file);
+						shadow_file = new_file->file;
+						r->files_count--;
+						free(prev->file);
+					} else {
+						free(prev->file);
+						prev->file = new_file->file;
+					}
+				} else {
+					// same as above but we keep the other string.
+					free(new_file->file);
+				}
+			}
+		}
+		free(shadow_file);
+		if (r->files_count < initial_files_count) {
+			size_t news = r->files_count * sizeof(RzBinSourceFile);
+			if (news / sizeof(RzBinSourceFile) == r->files_count) {
+				RzBinSourceFile *nf = realloc(r->files, news);
+				if (nf) {
+					r->files = nf;
+				}
+			}
+		}
+		rz_pvector_fini(&sorter);
+		free(initial_files); // no need to do anything with the strings inside, they are all moved or freed.
+	}
+
+	if (initial_lines_count) {
+		// Lines packing works slightly different than files.
+		// Samples at a certain address will never be dropped to extend a previous sample.
+		// That is because such a case does not seem very realistic for lines, as they are expected
+		// to fluctuate quite a lot whereas files often stay the same when functions from the same file
+		// are directly after each other.
+		RzPVector sorter;
+		rz_pvector_init(&sorter, NULL);
+		RzBinSourceLine *initial_lines = rz_vector_flush(&builder->lines);
+		rz_pvector_reserve(&sorter, initial_lines_count);
+		r->lines_count = 0;
+		for (size_t i = 0; i < initial_lines_count; i++) {
+			rz_pvector_push(&sorter, &initial_lines[i]);
+		}
+		rz_pvector_sort(&sorter, line_cmp);
+		for (size_t i = 0; i < initial_lines_count; i++) {
+			RzBinSourceLine *new_line = rz_pvector_at(&sorter, i);
+			if (!r->lines_count || r->lines[r->lines_count - 1].address != new_line->address) {
+				// new address, just move this entry to the final array
+				r->lines[r->lines_count++] = *new_line;
+			} else if (r->lines_count) {
+				// same address as the previous and we are not a closing sample, decide how to resolve this...
+				RzBinSourceLine *prev = &r->lines[r->lines_count - 1];
+				if (new_line->line > prev->line || (new_line->line == prev->line && new_line->column > prev->column)) {
+					// we supply the line!
+					// comparison is used to resolve non-determinism from the unstable (possibly randomized) quicksort.
+					*prev = *new_line;
+				}
+			}
+		}
+		if (r->lines_count < initial_lines_count) {
+			size_t news = r->lines_count * sizeof(RzBinSourceLine);
+			if (news / sizeof(RzBinSourceLine) == r->lines_count) {
+				RzBinSourceLine *nf = realloc(r->lines, news);
+				if (nf) {
+					r->lines = nf;
+				}
+			}
+		}
+		rz_pvector_fini(&sorter);
+		free(initial_lines);
+	}
+
+	rz_bin_source_line_info_builder_fini(builder);
+	return r;
+err_files:
+	free(r->files);
+err_r:
+	free(r);
+err:
+	rz_bin_source_line_info_builder_fini(builder);
+	return NULL;
+}
+
+RZ_API void rz_bin_source_line_info_free(RzBinSourceLineInfo *sli) {
+	if (!sli) {
+		return;
+	}
+	free(sli->lines);
+	free(sli->files);
+	free(sli);
+}
+
+#define binary_search_addr(r, prefix, addr) \
+	if (prefix##_count) { \
+		size_t l = 0; \
+		size_t h = prefix##_count; \
+		while (l < h - 1) { \
+			size_t m = l + ((h - l) >> 1); \
+			if (addr < prefix[m].address) { \
+				h = m; \
+			} else { \
+				l = m; \
+			} \
+		} \
+		r = (l < prefix##_count && prefix[l].address <= addr) ? &prefix[l] : NULL; \
+	}
+
+RZ_API const RzBinSourceFile *rz_bin_source_line_info_get_file_at(RzBinSourceLineInfo *sli, ut64 addr) {
+	RzBinSourceFile *r = NULL;
+	binary_search_addr(r, sli->files, addr);
+	if (r && r->file) {
+		// r->file == NULL would mean it's a closing entry which we don't want to return
+		return r;
+	}
+	return NULL;
+}
+
+RZ_API const RzBinSourceLine *rz_bin_source_line_info_get_line_at(RzBinSourceLineInfo *sli, ut64 addr) {
+	RzBinSourceLine *r = NULL;
+	binary_search_addr(r, sli->lines, addr);
+	if (r && r->line) {
+		// r->line == 0 would mean it's a closing entry which we don't want to return
+		return r;
+	}
+	return NULL;
 }
 
 RZ_API bool rz_bin_addr2line(RzBin *bin, ut64 addr, char *file, int len, int *line) {
---
+++ test/unit/test_bin_lines.c
@@ -0,0 +1,406 @@
+// SPDX-FileCopyrightText: 2021 thestr4ng3r <info@florianmaerkl.de>
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#include <rz_bin.h>
+#include "minunit.h"
+
+bool test_source_line_info_builder_empty() {
+	RzBinSourceLineInfoBuilder bob;
+	rz_bin_source_line_info_builder_init(&bob);
+	// add nothing
+	RzBinSourceLineInfo *li = rz_bin_source_line_info_builder_build_and_fini(&bob);
+	mu_assert_eq(li->files_count, 0, "files count");
+	mu_assert_null(li->files, "files");
+	mu_assert_eq(li->lines_count, 0, "lines count");
+	mu_assert_null(li->lines, "lines");
+	rz_bin_source_line_info_free(li);
+	mu_end;
+}
+
+bool test_source_line_info_builder() {
+#define FUZZ_COUNT 200
+	for (size_t f = 0; f < FUZZ_COUNT; f++) {
+#undef FUZZ_COUNT
+		RzBinSourceLineInfoBuilder bob;
+		rz_bin_source_line_info_builder_init(&bob);
+
+		// push the samples in random orders
+#define SAMPLES_COUNT 18
+		bool samples_applied[SAMPLES_COUNT] = { 0 };
+		for (size_t i = 0; i < SAMPLES_COUNT; i++) {
+			size_t j = rand() % SAMPLES_COUNT;
+			while (samples_applied[j]) {
+				j = (j + 1) % SAMPLES_COUNT;
+			}
+#undef SAMPLES_COUNT
+			samples_applied[j] = true;
+			switch (j) {
+			case 0:
+				rz_bin_source_line_info_builder_push_file_sample(&bob, 0x1000, "mayan.c");
+				break;
+			case 1:
+				rz_bin_source_line_info_builder_push_file_sample(&bob, 0x1005, NULL);
+				break;
+			case 2:
+				rz_bin_source_line_info_builder_push_file_sample(&bob, 0x1005, "mayan.c");
+				break;
+			case 3:
+				rz_bin_source_line_info_builder_push_file_sample(&bob, 0x1010, NULL);
+				break;
+			case 4:
+				rz_bin_source_line_info_builder_push_file_sample(&bob, 0x1010, "panoramas.c");
+				break;
+			case 5:
+				rz_bin_source_line_info_builder_push_file_sample(&bob, 0x1020, "pyramid.c");
+				break;
+			case 6:
+				rz_bin_source_line_info_builder_push_file_sample(&bob, 0x1020, "pyjamas.c");
+				break;
+			case 7:
+				rz_bin_source_line_info_builder_push_file_sample(&bob, 0x1080, NULL);
+				break;
+			case 8:
+				rz_bin_source_line_info_builder_push_file_sample(&bob, 0x1090, NULL);
+				break;
+			case 9:
+				rz_bin_source_line_info_builder_push_file_sample(&bob, 0x2000, "pyramania.c");
+				break;
+			case 10:
+				rz_bin_source_line_info_builder_push_line_sample(&bob, 0x1000, 42, 3);
+				break;
+			case 11:
+				rz_bin_source_line_info_builder_push_line_sample(&bob, 0x1001, 42, 5);
+				break;
+			case 12:
+				rz_bin_source_line_info_builder_push_line_sample(&bob, 0x1002, 1337, 1);
+				break;
+			case 13:
+				rz_bin_source_line_info_builder_push_line_sample(&bob, 0x1002, 123, 2);
+				break;
+			case 14:
+				rz_bin_source_line_info_builder_push_line_sample(&bob, 0x1005, 23, 0);
+				break;
+			case 15:
+				rz_bin_source_line_info_builder_push_line_sample(&bob, 0x1005, 0, 0);
+				break;
+			case 16:
+				rz_bin_source_line_info_builder_push_line_sample(&bob, 0x1100, 0, 0);
+				break;
+			case 17:
+				rz_bin_source_line_info_builder_push_line_sample(&bob, 0x1103, 2, 2);
+				break;
+			default:
+				break;
+			}
+		}
+
+		RzBinSourceLineInfo *li = rz_bin_source_line_info_builder_build_and_fini(&bob);
+
+		mu_assert_eq(li->files_count, 5, "files count");
+		mu_assert_notnull(li->files, "files");
+		mu_assert_eq(li->files[0].address, 0x1000, "file addr");
+		mu_assert_streq(li->files[0].file, "mayan.c", "file str");
+		mu_assert_eq(li->files[1].address, 0x1010, "file addr");
+		mu_assert_streq(li->files[1].file, "panoramas.c", "file str");
+		mu_assert_eq(li->files[2].address, 0x1020, "file addr");
+		// whether this is "pyjamas.c" or "pyramid.c" is implementation-dependent
+		// but it should be deterministic so we check for the concrete value.
+		mu_assert_streq(li->files[2].file, "pyramid.c", "file str");
+		mu_assert_eq(li->files[3].address, 0x1080, "file addr");
+		mu_assert_null(li->files[3].file, "file str");
+		mu_assert_eq(li->files[4].address, 0x2000, "file addr");
+		mu_assert_streq(li->files[4].file, "pyramania.c", "file str");
+
+		mu_assert_eq(li->lines_count, 6, "lines count");
+		mu_assert_notnull(li->lines, "lines");
+		mu_assert_eq(li->lines[0].address, 0x1000, "line addr");
+		mu_assert_eq(li->lines[0].line, 42, "line line");
+		mu_assert_eq(li->lines[0].column, 3, "line column");
+		mu_assert_eq(li->lines[1].address, 0x1001, "line addr");
+		mu_assert_eq(li->lines[1].line, 42, "line line");
+		mu_assert_eq(li->lines[1].column, 5, "line column");
+		// whether this is 123 or 1337 is implementation-dependent
+		// but it should be deterministic so we check for the concrete value.
+		mu_assert_eq(li->lines[2].address, 0x1002, "line addr");
+		mu_assert_eq(li->lines[2].line, 1337, "line line");
+		mu_assert_eq(li->lines[2].column, 1, "line column");
+		mu_assert_eq(li->lines[3].address, 0x1005, "line addr");
+		mu_assert_eq(li->lines[3].line, 23, "line line");
+		mu_assert_eq(li->lines[3].column, 0, "line column");
+		mu_assert_eq(li->lines[4].address, 0x1100, "line addr");
+		mu_assert_eq(li->lines[4].line, 0, "line line");
+		mu_assert_eq(li->lines[4].column, 0, "line column");
+		mu_assert_eq(li->lines[5].address, 0x1103, "line addr");
+		mu_assert_eq(li->lines[5].line, 2, "line line");
+		mu_assert_eq(li->lines[5].column, 2, "line column");
+
+		rz_bin_source_line_info_free(li);
+	}
+	mu_end;
+}
+
+bool test_source_line_info_builder_fuzz_lines() {
+#define FUZZ_COUNT 200
+	for (size_t f = 0; f < FUZZ_COUNT; f++) {
+#undef FUZZ_COUNT
+		RzBinSourceLineInfoBuilder bob;
+		rz_bin_source_line_info_builder_init(&bob);
+
+		// generate a lot of random samples and check them against a
+		// super slow but super simple equivalent algorithm
+#define SAMPLES_COUNT 0x200
+		RzBinSourceLine samples[SAMPLES_COUNT] = { 0 };
+		HtUP *unique_addrs = ht_up_new0();
+		size_t unique_addrs_count = 0;
+		for (size_t i = 0; i < SAMPLES_COUNT; i++) {
+			samples[i].address = rand() % 0x100;
+			if (rand() % 10 > 2) {
+				// non-closing entry
+				samples[i].line = rand() % 42;
+				samples[i].column = rand() % 42;
+			}
+			if (ht_up_insert(unique_addrs, samples[i].address, NULL)) {
+				unique_addrs_count++;
+			}
+			rz_bin_source_line_info_builder_push_line_sample(&bob, samples[i].address, samples[i].line, samples[i].column);
+		}
+		RzBinSourceLineInfo *li = rz_bin_source_line_info_builder_build_and_fini(&bob);
+
+		// resulting count should be exactly the number of unique addresses
+		mu_assert_eq(li->lines_count, unique_addrs_count, "lines count");
+		for (size_t i = 0; i < li->lines_count; i++) {
+			RzBinSourceLine *actual = &li->lines[i];
+			ut64 addr = actual->address;
+			mu_assert_true(!!ht_up_find_kv(unique_addrs, addr, NULL), "addr");
+			RzBinSourceLine *l = NULL;
+			for (size_t j = 0; j < SAMPLES_COUNT; j++) {
+				RzBinSourceLine *c = &samples[j];
+				if (c->address != addr) {
+					continue;
+				}
+				if (!l || c->line > l->line || (c->line == l->line && c->column > l->column)) {
+					l = c;
+				}
+			}
+			mu_assert_eq(actual->line, l->line, "line");
+			mu_assert_eq(actual->column, l->column, "column");
+		}
+
+		ht_up_free(unique_addrs);
+		rz_bin_source_line_info_free(li);
+	}
+#undef SAMPLES_COUNT
+	mu_end;
+}
+
+bool test_source_line_info_builder_fuzz_files() {
+	const char *const test_filenames[] = {
+		"into.c",
+		"the.c",
+		"black.c",
+		"wide.c",
+		"open.c"
+	};
+
+#define FUZZ_COUNT 200
+	for (size_t f = 0; f < FUZZ_COUNT; f++) {
+#undef FUZZ_COUNT
+		RzBinSourceLineInfoBuilder bob;
+		rz_bin_source_line_info_builder_init(&bob);
+
+#define SAMPLES_COUNT 0x200
+		RzBinSourceFile samples[SAMPLES_COUNT] = { 0 };
+		for (size_t i = 0; i < SAMPLES_COUNT; i++) {
+			samples[i].address = rand() % 0x100;
+			if (rand() % 10 > 2) {
+				// non-closing entry
+				samples[i].file = (char *)test_filenames[rand() % RZ_ARRAY_SIZE(test_filenames)];
+			}
+			rz_bin_source_line_info_builder_push_file_sample(&bob, samples[i].address, samples[i].file);
+		}
+		RzBinSourceLineInfo *li = rz_bin_source_line_info_builder_build_and_fini(&bob);
+
+		// every resulting sample must correspond to one of our original samples
+		// also there must be no duplicate addrs and, for optimization, no samples with the same contents after each other
+		for (size_t i = 0; i < li->files_count; i++) {
+			RzBinSourceFile *f = &li->files[i];
+			bool found = false;
+			for (size_t j = 0; j < SAMPLES_COUNT; j++) {
+				RzBinSourceFile *o = &samples[j];
+				if (o->address == f->address &&
+					((!o->file && !f->file) || (o->file && f->file && !strcmp(o->file, f->file)))) {
+					found = true;
+					break;
+				}
+			}
+			mu_assert_true(found, "resulting sample mapped back");
+
+			if (i) {
+				RzBinSourceFile *prev = &li->files[i - 1];
+				RzBinSourceFile *cur = &li->files[i];
+				mu_assert_true(prev->address < cur->address, "strictly increasing addr");
+				mu_assert_true(
+					(!prev->file != !cur->file) || (prev->file && cur->file && strcmp(prev->file, cur->file)),
+					"redundant entry");
+			}
+		}
+
+		// each of the original samples must be correctly represented, unless it was correctly overwritten
+		for (size_t i = 0; i < SAMPLES_COUNT; i++) {
+			RzBinSourceFile *o = &samples[i];
+			// check if o would be overwritten
+			for (size_t j = 0; j < SAMPLES_COUNT; j++) {
+				if (j == i) {
+					continue;
+				}
+				RzBinSourceFile *o2 = &samples[j];
+				if (o2->address != o->address) {
+					continue;
+				}
+				if ((!o->file && o2->file) || (o->file && o2->file && strcmp(o->file, o2->file) < 0)) {
+					o = NULL;
+					break;
+				}
+			}
+			if (!o) {
+				// overwritten
+				continue;
+			}
+
+			const RzBinSourceFile *f = rz_bin_source_line_info_get_file_at(li, o->address);
+			if (o->file) {
+				mu_assert_notnull(f, "sample");
+				mu_assert_true(f->address <= o->address, "sample addr");
+				mu_assert_notnull(f->file, "sample file");
+				mu_assert_streq(f->file, o->file, "sample file");
+			} else {
+				mu_assert_null(f, "sample");
+			}
+		}
+
+		rz_bin_source_line_info_free(li);
+	}
+#undef SAMPLES_COUNT
+	mu_end;
+}
+
+bool test_source_line_info_query() {
+	RzBinSourceFile files[] = {
+		{ 0x1000, strdup("mayan.c") },
+		{ 0x1010, strdup("panoramas.c") },
+		{ 0x1020, strdup("pyramid.c") },
+		{ 0x1080, NULL },
+		{ 0x2000, strdup("pyramania.c") }
+	};
+
+	RzBinSourceLine lines[] = {
+		{ 0x1000, 42, 3 },
+		{ 0x1001, 42, 5 },
+		{ 0x1002, 1337, 1 },
+		{ 0x1005, 23, 0 },
+		{ 0x1100, 0, 0 },
+		{ 0x1103, 2, 2 }
+	};
+
+	RzBinSourceLineInfo *li = RZ_NEW0(RzBinSourceLineInfo);
+	li->files = rz_mem_dup(files, sizeof(files));
+	li->files_count = RZ_ARRAY_SIZE(files);
+	li->lines = rz_mem_dup(lines, sizeof(lines));
+	li->lines_count = RZ_ARRAY_SIZE(lines);
+
+	const RzBinSourceFile *f = rz_bin_source_line_info_get_file_at(li, 0);
+	mu_assert_null(f, "file");
+	f = rz_bin_source_line_info_get_file_at(li, 0xfff);
+	mu_assert_null(f, "file");
+	f = rz_bin_source_line_info_get_file_at(li, 0x1000);
+	mu_assert_notnull(f, "file");
+	mu_assert_eq(f->address, 0x1000, "file addr");
+	mu_assert_streq(f->file, "mayan.c", "file str");
+	f = rz_bin_source_line_info_get_file_at(li, 0x100f);
+	mu_assert_notnull(f, "file");
+	mu_assert_eq(f->address, 0x1000, "file addr");
+	mu_assert_streq(f->file, "mayan.c", "file str");
+	f = rz_bin_source_line_info_get_file_at(li, 0x1010);
+	mu_assert_notnull(f, "file");
+	mu_assert_eq(f->address, 0x1010, "file addr");
+	mu_assert_streq(f->file, "panoramas.c", "file str");
+	f = rz_bin_source_line_info_get_file_at(li, 0x1030);
+	mu_assert_notnull(f, "file");
+	mu_assert_eq(f->address, 0x1020, "file addr");
+	mu_assert_streq(f->file, "pyramid.c", "file str");
+	f = rz_bin_source_line_info_get_file_at(li, 0x107f);
+	mu_assert_notnull(f, "file");
+	mu_assert_eq(f->address, 0x1020, "file addr");
+	mu_assert_streq(f->file, "pyramid.c", "file str");
+	f = rz_bin_source_line_info_get_file_at(li, 0x1080);
+	mu_assert_null(f, "file");
+	f = rz_bin_source_line_info_get_file_at(li, 0x1fff);
+	mu_assert_null(f, "file");
+	f = rz_bin_source_line_info_get_file_at(li, 0x2000);
+	mu_assert_notnull(f, "file");
+	mu_assert_eq(f->address, 0x2000, "file addr");
+	mu_assert_streq(f->file, "pyramania.c", "file str");
+	f = rz_bin_source_line_info_get_file_at(li, 0x500000);
+	mu_assert_notnull(f, "file");
+	mu_assert_eq(f->address, 0x2000, "file addr");
+	mu_assert_streq(f->file, "pyramania.c", "file str");
+
+	const RzBinSourceLine *l = rz_bin_source_line_info_get_line_at(li, 0);
+	mu_assert_null(l, "line");
+	l = rz_bin_source_line_info_get_line_at(li, 0xfff);
+	mu_assert_null(l, "line");
+	l = rz_bin_source_line_info_get_line_at(li, 0x1000);
+	mu_assert_notnull(l, "line");
+	mu_assert_eq(l->address, 0x1000, "line addr");
+	mu_assert_eq(l->line, 42, "line line");
+	mu_assert_eq(l->column, 3, "line column");
+	l = rz_bin_source_line_info_get_line_at(li, 0x1001);
+	mu_assert_notnull(l, "line");
+	mu_assert_eq(l->address, 0x1001, "line addr");
+	mu_assert_eq(l->line, 42, "line line");
+	mu_assert_eq(l->column, 5, "line column");
+	l = rz_bin_source_line_info_get_line_at(li, 0x1004);
+	mu_assert_notnull(l, "line");
+	mu_assert_eq(l->address, 0x1002, "line addr");
+	mu_assert_eq(l->line, 1337, "line line");
+	mu_assert_eq(l->column, 1, "line column");
+	l = rz_bin_source_line_info_get_line_at(li, 0x1005);
+	mu_assert_notnull(l, "line");
+	mu_assert_eq(l->address, 0x1005, "line addr");
+	mu_assert_eq(l->line, 23, "line line");
+	mu_assert_eq(l->column, 0, "line column");
+	l = rz_bin_source_line_info_get_line_at(li, 0x10ff);
+	mu_assert_notnull(l, "line");
+	mu_assert_eq(l->address, 0x1005, "line addr");
+	mu_assert_eq(l->line, 23, "line line");
+	mu_assert_eq(l->column, 0, "line column");
+	l = rz_bin_source_line_info_get_line_at(li, 0x1100);
+	mu_assert_null(l, "line");
+	l = rz_bin_source_line_info_get_line_at(li, 0x1102);
+	mu_assert_null(l, "line");
+	l = rz_bin_source_line_info_get_line_at(li, 0x1103);
+	mu_assert_notnull(l, "line");
+	mu_assert_eq(l->address, 0x1103, "line addr");
+	mu_assert_eq(l->line, 2, "line line");
+	mu_assert_eq(l->column, 2, "line column");
+	l = rz_bin_source_line_info_get_line_at(li, 0x424242);
+	mu_assert_notnull(l, "line");
+	mu_assert_eq(l->address, 0x1103, "line addr");
+	mu_assert_eq(l->line, 2, "line line");
+	mu_assert_eq(l->column, 2, "line column");
+
+	rz_bin_source_line_info_free(li);
+	mu_end;
+}
+
+bool all_tests() {
+	srand(time(0));
+	mu_run_test(test_source_line_info_builder_empty);
+	mu_run_test(test_source_line_info_builder_fuzz_lines);
+	mu_run_test(test_source_line_info_builder_fuzz_files);
+	mu_run_test(test_source_line_info_builder);
+	mu_run_test(test_source_line_info_query);
+	return tests_passed != tests_run;
+}
+
+mu_main(all_tests)
